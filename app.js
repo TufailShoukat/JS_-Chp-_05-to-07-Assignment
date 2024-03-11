5d96eacb27c04c0a91f886a26e4d25a0127bc56c
Tufail Shoukat <154624057+TufailShoukat@users.noreply.github.com>
2024-03-11 08:52:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,94 @@
+//Chapter-5  (All Questions)
+
+
+//===== Chapter-5 (Q-1) ====
+// var num = 13;
+// var num1 = 9;
+// var add = num + num1;
+// alert( add);
+
+
+//===== Chapter-5 (Q-2) ====
+
+
+// var num  = 13;
+// var num1  =  9;
+// var  sub  =  num - num1;
+// alert(sub);
+
+
+//===== Chapter-5 (Q-3) ===
+// var  num  =  13;
+// var  num1 =  9;
+// var  mul =    num * num1;
+// alert(mul);
+
+
+
+//===== Chapter-5 (Q-4) ====
+// var  num = 13;
+// var  num1  = 9;
+// var  div =  num / num1;
+// alert(div);
+
+
+//===== Chapter-5 (Q-5) ====
+// var num = 13;
+// var num1 = 9;
+// var mod = num % num1;
+// alert(mod);
+
+
+
+//Chapter-6  (All Questions)
+
+//===== Chapter-6 (Q-1) ====
+// var value = 
+// alert(value )
+// It is undefined (Value)
+
+
+
+//===== Chapter-6 (Q-2) ====
+// var value1 = 3;
+// alert(value1);
+// Now initial Value is (3)
+
+
+//===== Chapter-6 (Q-3) ====
+// var num =  3; + ++num;
+// alert(num);
+// Value after increment is (4)
+
+
+
+
+//Chapter-7  (All Questions)
+
+
+//===== Chapter-7 (Q-1) ====
+
+// var  value1 = 600;
+// var   value2  = 5 ;
+//  var  total =  value1 * value2;
+// // document.write(total);
+
+
+//===== Chapter-7 (Q-2) ====
+// var  table = "Table of (5)\n 5*1=5\n 5*2=10\n 5*3=15\n 5*4=20\n 5*5=25\n 5*6=30\n 5*7=35\n 5*8=40\n 5*9=45\n 5*10=50\n "
+// alert(table)
+
+//===== Chapter-7 (Q-3) ====
+// var total = 1200;
+// var  obt  =  900;
+// alert(total%obt) 
+// // var  marks = "<h1> Marks Sheet </h1>  <h1>Total Marks:1200</h1> <h1> Marks obtained:900</h1>  <h1> Percentage:75%</h1>";
+// // document.write(marks)
+
+
+//===== Chapter-7 (Q-4) ====
+// var current = 2024
+// var  birstDay=  2007
+// document.write( 2024 - 2007)
+//They are either 17 or 18 years old!
+
